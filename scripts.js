9a5ec1535617e6586f7fdb28bf089693cdeb2d11
Amit Kumar Mishra <amitmishraa651@gmail.com>
2025-02-13 16:45:27 +0530

    first commit

--- scripts.js
+++ scripts.js
@@ -1,76 +1,8 @@
-// function toggleMobileMenu(){
-// 	document.getElementById("menu").classList.toggle("active");
+function toggleMobileMenu(){
+	document.getElementById("menu").classList.toggle("active");
 	
-// }
+}
 
-
-
-
-// const menuBtn = document.getElementById("menu-btn");
-// const navLinks = document.getElementById("nav-links");
-// const menuBtnIcon = menuBtn.querySelector("i");
-
-// menuBtn.addEventListener("click", (e) => {
-//   navLinks.classList.toggle("open");
-
-//   const isOpen = navLinks.classList.contains("open");
-//   menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-// });
-
-// navLinks.addEventListener("click", (e) => {
-//   navLinks.classList.remove("open");
-//   menuBtnIcon.setAttribute("class", "ri-menu-line");
-// });
-
-// const scrollRevealOption = {
-//   distance: "50px",
-//   origin: "bottom",
-//   duration: 1000,
-// };
-
-// ScrollReveal().reveal(".header__image img", {
-//   ...scrollRevealOption,
-//   origin: "right",
-// });
-// ScrollReveal().reveal(".header__content h2", {
-//   ...scrollRevealOption,
-//   delay: 500,
-// });
-// ScrollReveal().reveal(".header__content h1", {
-//   ...scrollRevealOption,
-//   delay: 1000,
-// });
-// ScrollReveal().reveal(".header__content h3", {
-//   ...scrollRevealOption,
-//   delay: 1500,
-// });
-// ScrollReveal().reveal(".header__content p", {
-//   ...scrollRevealOption,
-//   delay: 2000,
-// });
-// ScrollReveal().reveal(".header__btns", {
-//   ...scrollRevealOption,
-//   delay: 2500,
-// });
-// ScrollReveal().reveal(" .section__subheader", {
-//   ...scrollRevealOption,
-//   delay: 500,
-// });
-// ScrollReveal().reveal(" .section__header", {
-//   ...scrollRevealOption,
-//   delay: 1000,
-// });
-
-// ScrollReveal().reveal(".journey__grid > div > div", {
-//   ...scrollRevealOption,
-//   interval: 500,
-// });
-
-// const mixer = mixitup(".portfolio__grid");
-
-// ScrollReveal().reveal(" .section__header", {
-//   ...scrollRevealOption,
-// });
 
 
 
